db792ce4c36e3301f74cbb1ea10b4aae6358e528
Daniel Mendes <128041054+danLima1@users.noreply.github.com>
2025-10-26 21:38:17 -0300

    correção do env

--- executor/executor_utils2.c
+++ executor/executor_utils2.c
@@ -30,6 +30,7 @@ void	setup_child_pipes(int prev_fd, int *pipe_fd, t_cmd *current)
 void	execute_child_process(t_cmd *current, t_shell *shell)
 {
 	char	*executable;
+	char	**envp;
 
 	if (handle_redirections(current->redirs) != 0)
 		exit(1);
@@ -38,8 +39,10 @@ void	execute_child_process(t_cmd *current, t_shell *shell)
 	else
 	{
 		executable = find_executable(current->args[0], shell->env);
-		if (execve(executable, current->args, NULL) == -1)
+		envp = env_to_array(shell->env);
+		if (execve(executable, current->args, envp) == -1)
 		{
+			free_env_array(envp);
 			perror("minishell");
 			exit(127);
 		}
