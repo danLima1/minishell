e3db71ea56810a72f4ee64ac8541f6aa59bc0c17
Daniel Mendes <128041054+danLima1@users.noreply.github.com>
2025-10-26 21:47:22 -0300

    novas na header

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldos-sa2 <ldos-sa2@student.42.rio>         +#+  +:+       +#+        */
+/*   By: ldos_sa2 <ldos-sa2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 08:18:07 by ldos_sa2          #+#    #+#             */
-/*   Updated: 2025/10/26 19:41:16 by ldos-sa2         ###   ########.fr       */
+/*   Updated: 2025/10/25 17:01:18 by ldos_sa2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ typedef enum e_token_type
 	TOKEN_REDIR_OUT,
 	TOKEN_REDIR_APPEND,
 	TOKEN_HEREDOC,
+	TOKEN_EOF
 }	t_token_type;
 
 typedef struct s_token
@@ -54,7 +55,6 @@ typedef struct s_redir
 {
 	t_token_type		type;
 	char				*file;
-	char				*eof;
 	struct s_redir		*next;
 }	t_redir;
 
@@ -102,8 +102,6 @@ t_cmd		*parse_tokens(t_token *tokens, t_shell *shell);
 void		free_cmd_list(t_cmd *cmd_list);
 void		print_cmd_list(t_cmd *cmd_list);
 int			count_commands(t_cmd *cmd);
-t_cmd		*create_cmd(void);
-void		handle_redd_arg(t_cmd *current_cmd, t_token *t);
 
 /* Expander  */
 char		*expand_string(char *str, t_shell *shell);
@@ -134,6 +132,8 @@ t_env		*init_env(char **envp);
 t_env		*create_env_node(char *key, char *value);
 void		add_env_node(t_env **env, t_env *new_node);
 void		free_env(t_env *env);
+char		**env_to_array(t_env *env);
+void		free_env_array(char **envp);
 
 /* Utils  */
 int			ft_strcmp(const char *s1, const char *s2);
